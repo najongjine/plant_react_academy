6c7cbd1d40011931f9242326fbb03fffbba918a1
najongjine <najongjin3@hotmail.com>
2025-08-20 14:41:32 +0900

    .

---
+++ src/components/WikiPreview.tsx
@@ -0,0 +1,14 @@
+// src/components/Header.tsx
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const WikiPreview: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default WikiPreview;
