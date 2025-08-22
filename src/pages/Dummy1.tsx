e29a3f101794fb6c0c584e867c91fde5a6629c8f
najongjine <najongjin3@hotmail.com>
2025-08-22 13:34:19 +0900

    .

--- src/pages/Dummy1.tsx
+++ src/pages/Dummy1.tsx
@@ -2,11 +2,18 @@
 import React, { useEffect, useRef, useState } from "react";
 
 const Dummy1: React.FC = () => {
-
+    const apiKey = process.env.REACT_APP_ROBOFLOW_API_KEY;
+    useEffect(
+        () => {
+            console.log(`# apikey:${apiKey}`);
+        }
+        , [])
 
     return (
         <div>
-
+            <div>
+                apikey:{apiKey}
+            </div>
         </div>
     );
 };
