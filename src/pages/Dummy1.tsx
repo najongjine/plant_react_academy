4db8afb5ea2e0e701b708ca24f06b794d15c8297
najongjine <najongjin3@hotmail.com>
2025-08-22 15:03:03 +0900

    .

--- src/pages/Dummy1.tsx
+++ src/pages/Dummy1.tsx
@@ -14,34 +14,43 @@ const Dummy1: React.FC = () => {
         , [])
 
     const getAIReponse = async () => {
-        console.log(`# apikey:${apiKey}`);
+        try {
 
-        const base64: string = await fileToBase64((file) as any);
-        const base64Body = base64.split(",")[1] ?? ""; // data:image/...;base64, 이후만 추출
+            const base64: string = await fileToBase64((file) as any);
+            const base64Body = base64.split(",")[1] ?? ""; // data:image/...;base64, 이후만 추출
 
-        // 2) roboflow 서버에 AI 판별 요청
-        const res: AxiosResponse<any> = await axios({
-            method: "POST",
-            url: `https://serverless.roboflow.com/plants-final/1`,
-            params: { api_key: apiKey },
-            data: base64Body,
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-        });
+            // 2) roboflow 서버에 AI 판별 요청
+            const res: AxiosResponse<any> = await axios({
+                method: "POST",
+                url: `https://serverless.roboflow.com/plants-final/1`,
+                params: { api_key: apiKey },
+                data: base64Body,
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            });
 
-        // RoboflowResponse 형태를 기대하지만, 혹시 몰라 any → 부분적 캐스팅
-        const data: any = res?.data;
-        console.log(`# data: `, data)
+            // RoboflowResponse 형태를 기대하지만, 혹시 몰라 any → 부분적 캐스팅
+            const data: any = res?.data;
+            console.log(`# data: `, data)
+        } catch (error: any) {
+            console.error(`! getAIReponse err: `, error?.message)
+        }
+
     }
 
     const onPick = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const f = e.target.files?.[0] || null;
-        setFile(f ?? null);
-        if (f) {
-            const url = URL.createObjectURL(f);
-        } else {
+        try {
+            const f = e.target.files?.[0] || null;
+            setFile(f ?? null);
+            if (f) {
+                const url = URL.createObjectURL(f);
+            } else {
+            }
+        } catch (error: any) {
+            console.error(`! onpick err: `, error?.message)
         }
+
     };
 
     return (
